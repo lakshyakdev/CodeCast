565ef7aec4e73b9b6df5c64afde8520c3a8af0b6
lakshyakdev <lakshyakdev@users.noreply.github.com>
2025-06-30 15:33:51 +0530

    updated cors policy

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ app.use(cookieParser(process.env.Secret));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://codecast-l16n.onrender.com",
   credentials: true,
 }));
 
